340e6d74133b0fab27ca45846d38b961d7c4428c
sailikotturu <saili7794@gmail.com>
2017-02-06 01:28:13 +0530

    First commit

--- app/src/main/java/com/example/sailik/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sailik/sampleapp/MainActivity.java
@@ -1,36 +1,32 @@
 package com.example.sailik.sampleapp;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends Activity {
-    private Button button_check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_check=(Button) findViewById(R.id.checkbutton);
-        button_check.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                EditText editText = (EditText) findViewById(R.id.edit_Text);
-                String response = editText.getText().toString();
-                TextView textview = (TextView) findViewById(R.id.textView2);
-                textview.setText(response);
-                intent.putExtra("STR_I_NEED", response);
-                startActivity(intent);
-            }
-        });
     }
 
+    public void selectFrag(View view) {
+        Fragment Frag;
+        if (view == findViewById(R.id.option1)) {
+            Frag = new CorrectFrag();
+        }else {
+            Frag = new WrongFrag();
+        }
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, Frag);
+        fragmentTransaction.commit();
 
 
+    }
 }
